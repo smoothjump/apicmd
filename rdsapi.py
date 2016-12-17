4db4f545786265111193177d8d58b14e46c54403
smoothjump <smoothjump@gmail.com>
2016-12-17 23:58:10 +0800

    slow log export

--- rdsapi.py
+++ rdsapi.py
@@ -15,6 +15,8 @@
 # privateIPAddress: 私网IP地址
 # dbInstanceId: RDS实例名
 # dbName: 数据库名称
+# startTime: 开始时间，格式为：YYYY-MM-DDTHH24:MMZ
+# endTime: 开始时间，格式为：YYYY-MM-DDTHH24:MMZ
 
 from aliyunsdkrds.request.v20140815 import DescribeBackupsRequest
 from aliyunsdkrds.request.v20140815 import DescribeDBInstanceAttributeRequest
@@ -22,14 +24,17 @@ from aliyunsdkrds.request.v20140815 import CreateUploadPathForSQLServerRequest
 from aliyunsdkrds.request.v20140815 import DescribeRegionsRequest
 from aliyunsdkrds.request.v20140815 import DescribeDBInstancesRequest
 from aliyunsdkrds.request.v20140815 import CreateReadOnlyDBInstanceRequest
+from aliyunsdkrds.request.v20140815 import DescribeSlowLogsRequest
+from aliyunsdkrds.request.v20140815 import DescribeSlowLogRecordsRequest
 from aliclient import AliClient
-import MySQLdb as db
 import json
 import urllib
 import types
 import os
 import sys
 import time
+import csv
+import xlwt
 
 class RDSClient(AliClient):
     client_conf = {}
@@ -37,39 +42,28 @@ class RDSClient(AliClient):
     def __init__(self, confFile):
         super(RDSClient,self).__init__(confFile)
 
-    def _init_conn(self):
-        with db.connect(host = self.client_conf.get("dbaas_host"),\
-        port =int(self.client_conf.get("dbaas_port")),\
-        user = self.client_conf.get("dbaas_user"),\
-        passwd = self.client_conf.get("dbaas_password"),db = "dbaas") as cur:
-            print "Initializing database connections,set charset to utf8...\nVersion of client library: %s" %(db.get_client_info())
-            cur.execute("set names utf8")
-            return cur
-
-    def _getName(self,dbInstanceId):
-        cur = self._init_conn()
-        stat = "select ins_name from cust_instance where is_deleted=0 and conn_addr = \'"+instance_name+"\' or ins_name = \'"+instance_name+"\' limit 1;"
-        print "%s" %stat
-        cur.execute(stat)
-        ins_name = cur.fetchone()[0]
-        cur.close()
-        return ins_name
-
     def DescribeDBInstance(self,dbInstanceId):
-        ins_name = self._getName(name)
         request = DescribeDBInstanceAttributeRequest.DescribeDBInstanceAttributeRequest()
         request.set_accept_format('json')
-        request.set_DBInstanceId(ins_name)
+        request.set_DBInstanceId(dbInstanceId)
         s = json.loads(self.clt.do_action(request))
         print s
 
-    def DescribeBackups(self,dbInstanceId,start_time,end_time):
-        ins_name = self._getName(name)
+    def DescribeSlowLogRecords(self, dbInstanceId, startTime, endTime, pageNumber):
+        request = DescribeSlowLogRecordsRequest.DescribeSlowLogRecordsRequest()
+        request.set_accept_format('json')
+        request.set_DBInstanceId(dbInstanceId)
+        request.set_StartTime(startTime)
+        request.set_EndTime(endTime)
+        request.set_PageNumber(pageNumber)
+        return json.loads(self.clt.do_action(request))
+
+    def DescribeBackups(self,dbInstanceId,startTime,endTime):
         request = DescribeBackupsRequest.DescribeBackupsRequest()
         request.set_accept_format('json')
-        request.set_DBInstanceId(ins_name)
-        request.set_StartTime(start_time+'T08:00Z')
-        request.set_EndTime(end_time+'T08:00Z')
+        request.set_DBInstanceId(dbInstanceId)
+        request.set_StartTime(startTime)
+        request.set_EndTime(endTime)
         s = json.loads(self.clt.do_action(request))
         result=[]
         for item in s['Items']['Backup']:
@@ -84,7 +78,7 @@ class RDSClient(AliClient):
     def CreateUploadPathForSQLServer(self, dbInstanceId,dbName):
         request = CreateUploadPathForSQLServerRequest.CreateUploadPathForSQLServerRequest() 
         request.set_accept_format('json')
-        request.set_DBInstanceId(ins_name)
+        request.set_DBInstanceId(dbInstanceId)
         request.set_DBName(db_name)
         print self.clt.do_action(request)
 
@@ -94,14 +88,43 @@ class RDSClient(AliClient):
         print self.clt.do_action(request)
 
     def CreateReadOnlyDBInstance(self,dbInstanceId,dbInstanceClass,dbInstanceStorage,engineVersion="5.6"):
-        ins_name = self._getName(dbInstanceId)
         request = CreateReadOnlyDBInstanceRequest.CreateReadOnlyDBInstanceRequest()
         request.set_accept_format('json')
-        request.set_DBInstanceId(ins_name)
+        request.set_DBInstanceId(dbInstanceId)
         request.set_EngineVrsion(version)
         request.set_DBInstanceClass(ins_cls)
         request.set_DBInstaceStorage(storage)
         print self.clt.do_action(request)
+
+    def getSlowLogs(self, dbInstanceId, startTime, endTime):
+        reload(sys)
+        sys.setdefaultencoding('utf-8')
+        s = self.DescribeSlowLogRecords(dbInstanceId, startTime, endTime, 1)
+        f = xlwt.Workbook()
+        j=0
+        table = f.add_sheet('slow_query',cell_overwrite_ok=True)
+        print "Dowloading slow query now, %d records in total" %(s["TotalRecordCount"])
+        totalPageCount = s["TotalRecordCount"]/30+1
+        table.write(j ,0,u"执行SQL用户及主机名")
+        table.write(j,1,u"数据库名称")
+        table.write(j,2,u"查询时长(秒)")
+        table.write(j,3,u'锁定时长(秒)')
+        table.write(j,4,u"开始执行时间")
+        table.write(j,5,u"SQL语句")
+        for i in xrange(totalPageCount):
+            s= self.DescribeSlowLogRecords(dbInstanceId, startTime, endTime, i+1)
+            j=i*30+1
+            for record in s["Items"]["SQLSlowRecord"]:
+                table.write(j ,0,record["HostAddress"])
+                table.write(j,1,record["DBName"])
+                table.write(j,2,record["QueryTimes"])
+                table.write(j,3,record["LockTimes"])
+                table.write(j,4,record["ExecutionStartTime"])
+                table.write(j,5,record["SQLText"])
+                j+=1
+            table.flush_row_data()
+        f.save("slow_query_"+dbInstanceId+".xls")
+        print "Download done, %d records in total" %(j-1)
 
 def downloadHook(block_read,block_size,total_size):
     if not block_read:
@@ -125,3 +148,7 @@ def dowloadFromURLs(urlList):
             urllib.urlretrieve(url,file_name,reporthook=downloadHook)
     else:
         print "Invalid argument!"
+
+if __name__=="__main__":
+    client = RDSClient("aliapi.ini")
+    client.getSlowLogs("XXXXXXXXX","2016-12-15T12:00Z","2016-12-16T13:00Z")
